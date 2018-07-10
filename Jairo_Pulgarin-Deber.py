deeb0e7cb41bd8ee8df01fb98bb50be539852744
Jairop1904 <jairop19040@gmail.com>
2018-07-10 17:56:10 -0500

    Programa que ingresado 2 números por teclado que vienen a ser la base y la altura de un triángulo se devuelve el área que tiene

---
+++ Jairo_Pulgarin-Deber.py
@@ -0,0 +1,12 @@
+print("    Universidad Israel")
+print("Nombre: JAiro Pulgarin")
+print("Curso: 7mo B")
+print("Programa que ingresando la base y la altura de un truango te calcula el area")
+
+b=float(input("Ingrese la base del Triangulo: "))
+h=float(input("Ingrese la haltura del Triangulo: "))
+
+A=(b*h)/2
+
+print("El Area del tringulo es: ", A)
+print("Gracias Por calcular con nosotros")
